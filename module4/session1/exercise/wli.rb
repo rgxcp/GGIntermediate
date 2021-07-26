417e0feed3af6aea5d04054f223b14d28fbea83b
Rommy Gustiawan <rgcppp@gmail.com>
2021-07-26 20:56:25 +0700

    Removing return statement

--- module4/session1/exercise/wli.rb
+++ module4/session1/exercise/wli.rb
@@ -8,15 +8,15 @@ class WLI
     def likes
         case @names.length
         when 0
-            return "No one likes this"
+            "No one likes this"
         when 1
-            return "#{@names.first} likes this"
+            "#{@names.first} likes this"
         when 2
-            return "#{@names.first} and #{@names.last} like this"
+            "#{@names.first} and #{@names.last} like this"
         when 3
-            return "#{@names.first}, #{@names[1]}, and #{@names[2]} like this"
+            "#{@names.first}, #{@names[1]}, and #{@names[2]} like this"
         else
-            return "#{@names.first}, #{@names[1]}, and #{@names.length - 2} others like this"
+            "#{@names.first}, #{@names[1]}, and #{@names.length - 2} others like this"
         end
     end
 end
